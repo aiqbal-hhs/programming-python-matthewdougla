edb57e558c7959f1716453b1f4d64c322beb98ac
matthewdougla <80366822+matthewdougla@users.noreply.github.com>
2021-05-06 14:56:52 +1200

    Add files via upload

---
+++ mattys.py
@@ -0,0 +1,13 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: //FS1.internal.hhs.school.nz/Home$/Students/matthew.douglas/Documents/year 10 dgt/year 10 term 2/matthew.py 
+matthew douglas
+matthew5
+what is your name? matthew
+matthew
+what is your age? 14
+ 14
+ what is your address? 200 parker road
+ 200 parker road
+>>> 
