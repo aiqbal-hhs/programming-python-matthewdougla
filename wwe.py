a18e568a6f4dccea0764fcfca288f1356cf5a7df
matthewdougla <80366822+matthewdougla@users.noreply.github.com>
2021-05-11 12:02:53 +1200

    Add files via upload

---
+++ wwe.py
@@ -0,0 +1,17 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: \\FS1.internal.hhs.school.nz\Home$\Students\matthew.douglas\Downloads\format-function-may10 (1).py 
+What is your name?jason
+jason
+what is your last name?statham
+statham
+How old are you?53
+53
+what is your birth date?26 july 1967
+26 july 1967
+where were you born?united kingdom
+united kingdom
+ how many siblings do you have?1
+53 is a nice age to be, jason!
+>>> 
