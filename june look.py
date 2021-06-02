7834e6c69661b1e7e92662b551424c8d240d1f6a
matthewdougla <80366822+matthewdougla@users.noreply.github.com>
2021-06-02 13:17:41 +1200

    Add files via upload

---
+++ june look.py
@@ -0,0 +1,32 @@
+#Print some maths statements
+print("9 x 12 =", 9 * 12)
+print("3 - 4 =" , 3 - 4)
+print("32 / 8 = " , 32 / 8)
+
+#Calculating the circumference of a circle
+PI = 3.14159
+diameter = 30
+
+#Hint: The circumference of a circle is pi times the diameter
+print("The circumference of the circle is:" , PI * diameter )
+
+
+
+
+hourly_rate = 15.50
+hours_worked = 40
+ 
+wages = hourly_rate * hours_worked
+ 
+print("This week you earned: $" , wages)
+
+
+
+
+
+hourly_rate = 15.50
+hours_worked = 40
+
+wages = hourly_rate * hours_worked
+
+print("This week you earned: $" + wages)
